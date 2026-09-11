e4ffba87bab671b4d3d4f50a5e5ed6b92d9a5196
Vasilii <vasekbasovv@mail.ru>
2026-09-11 16:02:48 +0300

    step 1A: ArrayList vs LinkedList benchmark (micro-step A done)

--- src/main/java/ru/algobank/algo/step01/ListBenchmark.java
+++ src/main/java/ru/algobank/algo/step01/ListBenchmark.java
@@ -1,4 +1,46 @@
 package ru.algobank.algo.step01;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Timer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class ListBenchmark {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = IntStream.range(0,100_000).boxed().collect(Collectors.toCollection(ArrayList::new));
+        LinkedList<Integer> linkedList = IntStream.range(0,100_000).boxed().collect(Collectors.toCollection(LinkedList::new));
+
+        System.out.println("ArrayList: " + getCenter1k(list) + " | " + addLast1k(list) + " | " + addStart1k(list));
+        System.out.println("LinkedList: " + getCenter1k(linkedList) + " | " + addLast1k(linkedList) + " | " + addStart1k(linkedList));
+    }
+
+    public static String getCenter1k(List<Integer> list){
+        long time = System.nanoTime();
+        for (int i = 0; i < 1000; i++) {
+            list.get(50000);
+        }
+        long elapsed = System.nanoTime() - time;
+        return "get(50000)=" + (elapsed/1000) + " мкс";
+    }
+
+    public static String addLast1k(List<Integer> list){
+        long time = System.nanoTime();
+        for (int i = 0; i < 1000; i++) {
+            list.add(1);
+        }
+        long elapsed = System.nanoTime() - time;
+        return "addLast=" + (elapsed/1000) + " мкс";
+    }
+
+    public static String addStart1k(List<Integer> list){
+        long time = System.nanoTime();
+        for (int i = 0; i < 1000; i++) {
+            list.add(0, 1);
+        }
+        long elapsed = System.nanoTime() - time;
+        return "addStart1k time: " + (elapsed/1000) + " мкс";
+    }
 }
