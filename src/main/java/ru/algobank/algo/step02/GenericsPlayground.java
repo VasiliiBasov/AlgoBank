1dc55abce3fb6fb3889a400c82da1c8ae65f381f
Vasilii <vasekbasovv@mail.ru>
2026-09-12 19:57:45 +0300

    step 2A: Generics playground (?, ? extends, ? super) - bug in PECS

---
+++ src/main/java/ru/algobank/algo/step02/GenericsPlayground.java
@@ -0,0 +1,34 @@
+package ru.algobank.algo.step02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericsPlayground {
+
+    public static void printList(List<?> list) {
+        list.forEach(System.out::println);
+    }
+
+    public static double sumNumbers(List<? extends Number> numbers) {
+        return numbers.stream().mapToDouble(Number::doubleValue).sum();
+    }
+
+    public static void addIntegers(List<? super Number> sink) {
+        sink.add(1);
+        sink.add(2);
+        sink.add(3);
+    }
+
+    public static void main(String[] args) {
+        List<String> strings = List.of("alice", "bob", "carol");
+        List<Integer> ints = List.of(1, 2, 3, 4, 5);
+        List<Double> doubles = List.of(1.5, 2.5, 3.0);
+        List<Number> numbers = new ArrayList<>();
+
+        printList(strings);
+        System.out.println("Сумма чисел ints: " + sumNumbers(ints));
+        System.out.println("Сумма чисел doubles: " + sumNumbers(doubles));
+        addIntegers(numbers);
+        System.out.println("Numbers: " + numbers);
+    }
+}
